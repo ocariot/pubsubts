9820efe104ced50df05d1c79b7a2ceb3b20bd6f0
Lucas Barbosa <lucas.barbosa@nutes.uepb.edu.br>
2019-08-29 20:22:00 -0300

    Removing .js

--- tutorials/1_publish_subscribe/publish.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var index_1 = require("../../index");
-var ocariot = new index_1.RabbitMQClient('Account');
-ocariot.pubSavePhysicalActivity({ activity: 'Saving Activity...' })["catch"](function (err) {
-    console.log(err);
-});
-setTimeout(ocariot.close, 3000);
--- tutorials/1_publish_subscribe/subscribe.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var index_1 = require("../../index");
-var ocariot = new index_1.RabbitMQClient('Account');
-ocariot.subSavePhysicalActivity(function (message) {
-    console.log(message);
-});
