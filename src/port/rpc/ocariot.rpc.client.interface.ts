82c308f3a562b72aee50ec955f891191a59cc563
Douglas Rafael <douglas.rafael@nutes.uepb.edu.br>
2019-10-17 19:25:31 -0300

    Updated RPC Server and Client functions.
    - Changed getLogs(query: string): Promise<any> to getLogs(childId: string, query?: string): Promise<any>
    - Now query parameter on all server and client functions is optional.

--- src/port/rpc/ocariot.rpc.client.interface.ts
+++ src/port/rpc/ocariot.rpc.client.interface.ts
@@ -3,31 +3,31 @@ import { IRpcClient } from './rpc.client.interface'
 export interface IOcariotRpcClient extends IRpcClient {
     getPhysicalActivities(query: string, callback: (err, result) => void): void
 
-    getPhysicalActivities(query: string): Promise<any>
+    getPhysicalActivities(query?: string): Promise<any>
 
     getSleep(query: string, callback: (err, result) => void): void
 
-    getSleep(query: string): Promise<any>
+    getSleep(query?: string): Promise<any>
 
     getWeights(query: string, callback: (err, result) => void): void
 
-    getWeights(query: string): Promise<any>
+    getWeights(query?: string): Promise<any>
 
     getEnvironments(query: string, callback: (err, result) => void): void
 
-    getEnvironments(query: string): Promise<any>
+    getEnvironments(query?: string): Promise<any>
 
-    getLogs(query: string, callback: (err, result) => void): void
+    getLogs(childId: string, query: string, callback: (err, result) => void): void
 
-    getLogs(query: string): Promise<any>
+    getLogs(childId: string, query?: string): Promise<any>
 
     getChildren(query: string, callback: (err, children) => void): void
 
-    getChildren(query: string): Promise<any>
+    getChildren(query?: string): Promise<any>
 
     getFamilies(query: string, callback: (err, families) => void): void
 
-    getFamilies(query: string): Promise<any>
+    getFamilies(query?: string): Promise<any>
 
     getFamilyChildren(familyId: string, callback: (err, children) => void): void
 
@@ -35,7 +35,7 @@ export interface IOcariotRpcClient extends IRpcClient {
 
     getEducators(query: string, callback: (err, educators) => void): void
 
-    getEducators(query: string): Promise<any>
+    getEducators(query?: string): Promise<any>
 
     getEducatorChildrenGroups(educatorId: string, callback: (err, childrenGroups) => void): void
 
@@ -43,7 +43,7 @@ export interface IOcariotRpcClient extends IRpcClient {
 
     getHealthProfessionals(query: string, callback: (err, healthProfessionals) => void): void
 
-    getHealthProfessionals(query: string): Promise<any>
+    getHealthProfessionals(query?: string): Promise<any>
 
     getHealthProfessionalChildrenGroups(healthProfessionalId: string, callback: (err, childrenGroups) => void): void
 
@@ -51,9 +51,9 @@ export interface IOcariotRpcClient extends IRpcClient {
 
     getApplications(query: string, callback: (err, applications) => void): void
 
-    getApplications(query: string): Promise<any>
+    getApplications(query?: string): Promise<any>
 
     getInstitutions(query: string, callback: (err, institutions) => void): void
 
-    getInstitutions(query: string): Promise<any>
+    getInstitutions(query?: string): Promise<any>
 }
--- src/port/rpc/ocariot.rpc.server.interface.ts
+++ src/port/rpc/ocariot.rpc.server.interface.ts
@@ -1,31 +1,31 @@
 import { IRpcServer } from './rpc.server.interface'
 
 export interface IOcariotRpcServer extends IRpcServer {
-    providePhysicalActivities(listener: (query: string) => any): Promise<void>
+    providePhysicalActivities(listener: (query?: string) => any): Promise<void>
 
-    provideLogs(listener: (query: string) => any): Promise<void>
+    provideLogs(listener: (childId: string, query?: string) => any): Promise<void>
 
-    provideSleep(listener: (query: string) => any): Promise<void>
+    provideSleep(listener: (query?: string) => any): Promise<void>
 
-    provideWeights(listener: (query: string) => any): Promise<void>
+    provideWeights(listener: (query?: string) => any): Promise<void>
 
-    provideEnvironments(listener: (query: string) => any): Promise<void>
+    provideEnvironments(listener: (query?: string) => any): Promise<void>
 
-    provideChildren(listener: (query: string) => any): Promise<void>
+    provideChildren(listener: (query?: string) => any): Promise<void>
 
-    provideFamilies(listener: (query: string) => any): Promise<void>
+    provideFamilies(listener: (query?: string) => any): Promise<void>
 
     provideFamilyChildren(listener: (familyId: string) => any): Promise<void>
 
-    provideEducators(listener: (query: string) => any): Promise<void>
+    provideEducators(listener: (query?: string) => any): Promise<void>
 
     provideEducatorChildrenGroups(listener: (educatorId: string) => any): Promise<void>
 
-    provideHealthProfessionals(listener: (query: string) => any): Promise<void>
+    provideHealthProfessionals(listener: (query?: string) => any): Promise<void>
 
     provideHealthProfessionalChildrenGroups(listener: (healthProfessionalId: string) => any): Promise<void>
 
-    provideApplications(listener: (query: string) => any): Promise<void>
+    provideApplications(listener: (query?: string) => any): Promise<void>
 
-    provideInstitutions(listener: (query: string) => any): Promise<void>
+    provideInstitutions(listener: (query?: string) => any): Promise<void>
 }
